185300f9529dce4bed1ef7d3e829adac9742b482
Romain.S <95850189+SOULIERomain@users.noreply.github.com>
2022-11-15 16:56:36 +0100

    Ajout du projet WPF

--- MegaCasting2022.pgr/Program.cs
+++ MegaCasting2022.pgr/Program.cs
@@ -4,28 +4,22 @@ MegaCastingCsharpContext context = new MegaCastingCsharpContext();
 
 context.Activities.ToList().ForEach(activity => Console.WriteLine(activity.Identifier + " - " + activity.Name));
 
-Console.WriteLine("Voulez-vous ajouter (1) ou supprimer (2) des données de cette liste ?");
+// Ajout
+context.Activities
+    .Add(new Activity() { Name = "Doc" });
+context.SaveChanges();
 
-switch (Console.ReadLine())
-{
-	case "1":
-		Console.Clear();
-        context.Activities.ToList().ForEach(activity => Console.WriteLine(activity.Identifier + " - " + activity.Name));
-        Console.WriteLine("Quel est l'identifiant de l'activité à supprimer ?");
-		string entree = Console.ReadLine();
-		foreach (Activity act in context.Activities)
-		{
-			if(act.Identifier.ToString() == entree)
-			{
-				Console.WriteLine(context.Activities.Where(activity => activity.Identifier.ToString() == entree));
-			}
-		}
-		Console.ReadLine();
-		break;
-	case "2":
-		break;
-	case "0":
-		break;
-	default:
-		break;
-}
+// Suppression
+context.Activities.Remove(context.Activities
+    .OrderBy(civ => civ.Identifier)
+    .Last());
+
+// Modification
+Activity activity = context.Activities
+    .OrderByDescending(civ => civ.Identifier)
+    .First();
+
+activity.Name = "Docteur";
+context.SaveChanges();
+
+Console.ReadKey();
